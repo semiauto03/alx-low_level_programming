69a316d57f62e132e1e0704cea538f13111fe271
semiauto03 <semiaut03@gmail.com>
2022-12-13 21:19:08 -0800

    hello world tasks

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print a code and leave a new line
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,5 @@
+Task 0- preprocessor
+Task 1 -  compiler
+Task 2 - Assembler
+TAsk 3 - Name
+Task 4 - Heelo, puts
