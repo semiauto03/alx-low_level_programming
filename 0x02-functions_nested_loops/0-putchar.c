8e62b23621fb035581955789a0615a3804480f41
semiauto03 <semiaut03@gmail.com>
2022-12-18 20:10:07 -0800

    updated task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,23 +2,18 @@
 
 /**
  * main - Print _putchar
- * is written by semiauto
  * Return: Always 0
  */
-
 int main(void)
-
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
-{
-}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
